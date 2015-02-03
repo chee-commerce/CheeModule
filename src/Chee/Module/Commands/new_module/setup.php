44ba7b6fe38f4cc839e14eced070d938f105c267
Mohsen Shafiei <mohsen.sh12@hotmail.com>
2015-02-03 12:42:05 +0330

    Fix error in module template

--- src/Chee/Module/Commands/new_module/setup.php
+++ src/Chee/Module/Commands/new_module/setup.php
@@ -10,13 +10,11 @@ class Setup
 
     public function subscribe($events)
     {
-        $events->listen('modules.install.' . $this->name, 'Modules\#moduleName\#moduleName@install');
-        $events->listen('modules.uninstall.' . $this->name, 'Modules\#moduleName\#moduleName@uninstall');
-        $events->listen('modules.delete.' . $this->name, 'Modules\#moduleName\#moduleName@delete');
-        $events->listen('modules.enable.' . $this->name, 'Modules\#moduleName\#moduleName@enable');
-        $events->listen('modules.disable.' . $this->name, 'Modules\#moduleName\#moduleName@disable');
-        $events->listen('modules.reset.' . $this->name, 'Modules\#moduleName\#moduleName@reset');
-        $events->listen('modules.update.' . $this->name, 'Modules\#moduleName\#moduleName@update');
+        $events->listen('modules.install.' . $this->name, 'Modules\#moduleName\Setup@install');
+        $events->listen('modules.uninstall.' . $this->name, 'Modules\#moduleName\Setup@uninstall');
+        $events->listen('modules.enable.' . $this->name, 'Modules\#moduleName\Setup@enable');
+        $events->listen('modules.disable.' . $this->name, 'Modules\#moduleName\Setup@disable');
+        $events->listen('modules.update.' . $this->name, 'Modules\#moduleName\Setup@update');
     }
 
     public function install($event)
@@ -29,22 +27,12 @@ class Setup
 
     }
 
-    public function delete($event)
-    {
-
-    }
-
     public function disable($event)
     {
 
     }
 
     public function enable($event)
-    {
-
-    }
-
-    public function reset($event)
     {
 
     }
